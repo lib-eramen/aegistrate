7a38e6b574fcabb8771f1e3e0d297cdcb6761f19
Ramen Developer <ramendev2009@gmail.com>
2023-05-20 17:51:51 +0700

    PROGRESS: Base for development

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+#![deny(unused_must_use, unused_imports, rust_2018_idioms)]
+#![warn(clippy::all, clippy::pedantic)]
+#![allow(missing_docs)]
+
+pub type Aegis<T> = anyhow::Result<T>;
+
+#[tokio::main]
+async fn main() -> Aegis<()> {
+    std::env::set_var("RUST_LOG", "serenity=debug,sentinel=trace");
+    dotenv::dotenv()?;
+    env_logger::init();
     println!("Hello, world!");
+    Ok(())
 }
